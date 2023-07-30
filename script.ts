2e5b8dad0fb1968e44f65781b24134e66eb01681
Matthew <mattklawitt@gmail.com>
2023-07-30 13:49:00 -0400

    Updated schema relations, added test queries

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, rarity } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -28,6 +28,79 @@ async function main() {
   });
 
   console.log(user);
+
+  const cardset = await prisma.cardSet.create({
+    data: {
+      setName: 'TestCardSet',
+    },
+  });
+
+  const card = await prisma.card.create({
+    data: {
+      name: 'TestCard',
+      desc: 'This card does something',
+      rarity: rarity.common,
+      cardSet: {
+        connect: {
+          id: cardset.id,
+        },
+      },
+    },
+  });
+
+  const card2 = await prisma.card.create({
+    data: {
+      name: 'TestCard2',
+      desc: 'This card does something too',
+      rarity: rarity.common,
+      cardSet: {
+        connect: {
+          id: cardset.id,
+        },
+      },
+    },
+  });
+
+  console.log(cardset);
+
+  const card_collection = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      cardCollection: {
+        create: {
+          cardId: card.id,
+          quantity: 1,
+        },
+      },
+    },
+  });
+
+  const card_collection2 = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      cardCollection: {
+        create: {
+          cardId: card2.id,
+          quantity: 1,
+        },
+      },
+    },
+  });
+
+  await prisma.cardCollection.update({
+    where: {
+      id: card_collection2.id,
+    },
+    data: {
+      quantity: { increment: 1 },
+    },
+  });
+
+  console.log(card_collection);
 }
 
 main()
